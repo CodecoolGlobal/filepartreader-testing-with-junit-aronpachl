fd6292a9c7f351da3e5977fe063a12c970f852ec
aron <pachl.aron@gmail.com>
2020-05-23 15:56:26 +0200

    getWordsOrderedAlphabetically, getWordsContainingSubstring, getStringWhichPalindromes methods created

---
+++ src/main/java/com/codecool/file_part_reader/FileWordAnalyzer.java
@@ -0,0 +1,54 @@
+package com.codecool.file_part_reader;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FileWordAnalyzer {
+
+    FilePartReader filePartReader;
+
+    public FileWordAnalyzer(FilePartReader filePartReader) {
+        this.filePartReader = filePartReader;
+    }
+
+    public List<String> getWordsOrderedAlphabetically(){
+        List<String> wordsList = returnWordsFromLines();
+        Collections.sort(wordsList);
+        return wordsList;
+    }
+
+
+    public List<String> getWordsContainingSubstring(String subString){
+        List<String> wordsList = returnWordsFromLines();
+        List<String> result = new ArrayList<>();
+        for(String word : wordsList){
+            if (word.contains(subString)){
+                result.add(word);
+            }
+        }
+        return result;
+    }
+
+    public List<String> getStringsWhichPalindromes(){
+        List<String> wordsList = returnWordsFromLines();
+        List<String> result = new ArrayList<>();
+        for(String word : wordsList){
+            String reversed = "";
+            for(int i = word.length() -1; i >= 0; i--){
+                reversed = reversed + word.charAt(i);
+            }
+            if(word.equals(reversed)){
+                result.add(word);
+            }
+        }
+        return result;
+    }
+
+    public List<String> returnWordsFromLines(){
+        String lines = filePartReader.readLines("text.txt", 2, 5);
+        String[] words = lines.split("");
+        return Arrays.asList(words);
+    }
+}
