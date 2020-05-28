c8be72286d3ea9d4732503a3a2f77a23779e3e13
aron <pachl.aron@gmail.com>
2020-05-28 11:28:21 +0200

    corrected setup, read, readLines method

--- src/main/java/com/codecool/file_part_reader/FilePartReader.java
+++ src/main/java/com/codecool/file_part_reader/FilePartReader.java
@@ -3,30 +3,39 @@ package com.codecool.file_part_reader;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FilePartReader {
 
+    String filePath;
+    int fromLine;
+    int toLine;
+
     public void setup(String filePath, int fromLine, int toLine) {
 
+        this.filePath = filePath;
+
         if (toLine < fromLine) throw  new IllegalArgumentException("toLine is smaller");
+        else this.toLine = toLine;
 
         if (fromLine < 1) throw new IllegalArgumentException("fromLine can't be lower than 1");
-
+        else this.fromLine = fromLine;
     }
 
-    public ArrayList<String> read(String filePath) throws IOException {
-        ArrayList<String> records = new ArrayList<String>();
+    public String read() throws IOException {
+        StringBuilder text = new StringBuilder();
         try
         {
             BufferedReader reader = new BufferedReader(new FileReader(filePath));
             String line;
             while ((line = reader.readLine()) != null)
             {
-                records.add(line);
+                text.append(line);
+                text.append(" ");
             }
             reader.close();
-            return records;
+            return text.toString();
         } catch (IOException e) {
             throw  new IOException("file not found!");
         }
@@ -34,12 +43,13 @@ public class FilePartReader {
 
     }
 
-    public String readLines(String filePath, int fromLine, int toLine) throws IOException {
+    public String readLines() throws IOException {
         StringBuilder result = new StringBuilder();
-        ArrayList<String> file = read(filePath);
+        List<String> file = Arrays.asList(read().split(" "));
 
         for (int line = fromLine; line < toLine; line++ ){
             result.append(file.get(line));
+            result.append(" ");
         }
         return result.toString();
     }
