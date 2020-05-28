a219f60912150d12d98ecf4de3a29235c32c4d76
aron <pachl.aron@gmail.com>
2020-05-28 12:45:19 +0200

    correcting tests and added a new

--- src/main/java/com/codecool/FilePartReaderTest.java
+++ src/main/java/com/codecool/FilePartReaderTest.java
@@ -4,28 +4,39 @@ import com.codecool.file_part_reader.FilePartReader;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilePartReaderTest {
 
     @Test
-    public void testIsReadFileThrowsIOExcepction(){
+    public void testIsReadFileThrowsIOException(){
         final FilePartReader filePartReader = new FilePartReader();
-        assertThrows(IOException.class, () -> filePartReader.read("text.txt"));
+        filePartReader.setup("text.txt", 4,7);
+        assertThrows(IOException.class, filePartReader::read);
     }
 
     @Test
     public void testIsFromLineBelow1ThrowsIllegalArgumentException(){
         final FilePartReader filePartReader = new FilePartReader();
-        assertThrows(IllegalArgumentException.class, () -> filePartReader.setup("text.txt", 0, 5));
+        assertThrows(IllegalArgumentException.class, () -> filePartReader.setup("src/main/resources/text.txt", 0, 5));
     }
 
 
     @Test
     public void testIsToLineBelowFromLineThrowsIllegalArgumentException(){
         final FilePartReader filePartReader = new FilePartReader();
-        assertThrows(IllegalArgumentException.class, () -> filePartReader.setup("text.txt", 5, 2));
+        assertThrows(IllegalArgumentException.class, () -> filePartReader.setup("src/main/resources/text.txt", 5, 2));
+    }
+
+    @Test
+    public void testReadLinesGetCorrectSize() throws IOException {
+        final FilePartReader filePartReader = new FilePartReader();
+        filePartReader.setup("src/main/resources/text.txt", 4,7);
+        List<String> textWords = Arrays.asList(filePartReader.readLines().split(" "));
+        assertEquals(3, textWords.size());
     }
 
 }
